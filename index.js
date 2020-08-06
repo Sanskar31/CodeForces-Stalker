dc2a29193c8d71e710bf0b614568fdd7bb4c85d1
Sanskar Agarwal <sanskaragarwal05@gmail.com>
2020-08-06 12:59:36 +0530

    Added 404 Page

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/*
-All Imports
-*/
+//All Imports
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -9,15 +7,13 @@ const app = express();
 
 var port = process.env.PORT || 3000;
 
-/*
-Setting View Engine
-*/
+const userRoutes = require("./routes/userRoutes");
+
+//Setting View Engine & static folders
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
-
-const userRoutes = require("./routes/userRoutes");
 
 app.use(userRoutes);
 
@@ -28,6 +24,11 @@ app.get("/", (req, res, next) => {
 	});
 });
 
+//Error Page
+app.use((req, res, next) => {
+	res.status(404).render("error-404");
+});
+
 app.listen(port, () => {
-	console.log("Connected On Port " + port + "!");
+	console.log(`Connected On Port ${port}!`);
 });
